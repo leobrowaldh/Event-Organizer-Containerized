813c098ef014eaf8e684988a09c793322833dfa7
leobrowaldh <leobrowaldh@gmail.com>
2024-09-10 12:04:52 +0200

    changing database delete behavior

--- Data/Migrations/20240910100401_change-delete-behaviours-again.Designer.cs
+++ Data/Migrations/20240910100401_change-delete-behaviours-again.Designer.cs
@@ -4,6 +4,7 @@ using Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Migrations
 {
     [DbContext(typeof(EventOrganizerDbContext))]
-    partial class EventOrganizerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240910100401_change-delete-behaviours-again")]
+    partial class changedeletebehavioursagain
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20240910100401_change-delete-behaviours-again.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedeletebehavioursagain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Activities_ActivityId",
+                table: "Users");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Events_EventId",
+                table: "Users");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Activities_ActivityId",
+                table: "Users",
+                column: "ActivityId",
+                principalTable: "Activities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Events_EventId",
+                table: "Users",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Activities_ActivityId",
+                table: "Users");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Events_EventId",
+                table: "Users");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Activities_ActivityId",
+                table: "Users",
+                column: "ActivityId",
+                principalTable: "Activities",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Events_EventId",
+                table: "Users",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
