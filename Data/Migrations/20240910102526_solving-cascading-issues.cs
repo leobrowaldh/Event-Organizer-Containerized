5ad0afed49acbea422b92397ab73aeb757bdf12d
leobrowaldh <leobrowaldh@gmail.com>
2024-09-10 12:27:44 +0200

    trying other cascading combo

---
+++ Data/Migrations/20240910102526_solving-cascading-issues.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class solvingcascadingissues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Activities_ActivityId",
+                table: "Users");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Activities_ActivityId",
+                table: "Users",
+                column: "ActivityId",
+                principalTable: "Activities",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Activities_ActivityId",
+                table: "Users");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Activities_ActivityId",
+                table: "Users",
+                column: "ActivityId",
+                principalTable: "Activities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
