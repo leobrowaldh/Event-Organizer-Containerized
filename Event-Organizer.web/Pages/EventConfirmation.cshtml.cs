8a75721bad0d3899b5be1a746b3f6c1aff1d83e2
leobrowaldh <leobrowaldh@gmail.com>
2024-09-18 15:34:39 +0200

    tutorial now shows statically in the html instead of the modal display, both in index and in eventConfirmation

--- Event-Organizer.web/Pages/EventConfirmation.cshtml.cs
+++ Event-Organizer.web/Pages/EventConfirmation.cshtml.cs
@@ -5,7 +5,6 @@ namespace Event_Organizer.web.Pages
 {
     public class EventConfirmationModel : PageModel
     {
-        public bool FirstTimeCreatingEvent { get; set; }
         public string? EventLink { get; set; }
         public Guid? EventId { get; set; }
 
@@ -14,27 +13,6 @@ namespace Event_Organizer.web.Pages
             // Retrieve TempData values
             EventLink = TempData["EventLink"] as string;
             EventId = TempData["EventId"] as Guid?;
-
-            // Check if the "FirstTimeCreatingEvent" cookie exists
-            var cookie = Request.Cookies["FirstTimeCreatingEvent"];
-
-            if (string.IsNullOrEmpty(cookie))
-            {
-                // If the cookie doesn't exist, it's the user's first visit
-                FirstTimeCreatingEvent = true;
-
-                // Set the "FirstVisit" cookie with a value and expiration date
-                var cookieOptions = new CookieOptions
-                {
-                    Expires = DateTime.Now.AddYears(1) // Set the cookie to expire in 1 year
-                };
-                Response.Cookies.Append("FirstTimeCreatingEvent", "true", cookieOptions);
-            }
-            else
-            {
-                // If the cookie exists, it's not the first visit
-                FirstTimeCreatingEvent = false;
-            }
         }
     }
 }
